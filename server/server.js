d01d2fcbb0093a78dc969646ad9a6498b94c1807
Ted Perez <taperez1989@yahoo.com>
2024-10-31 18:56:50 -0700

    start of stripe installation

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 require('dotenv').config();
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
-// imports Apollo/server from apollo library
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
-// importing typeDefs and resolvers from the schema file
 const db = require("./config/connection");
 const { Funko } = require("./models");
 const { authMiddleware } = require('./utils/auth');
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Use environment variable for the secret key
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,22 +17,40 @@ const server = new ApolloServer({
 });
 
 const startApolloServer = async () => {
-
   await server.start();
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
+  // GraphQL middleware
   app.use("/graphql", expressMiddleware(server, {
     context: authMiddleware
   }));
+
+  // Payment intent route
+  app.post('/create-payment-intent', async (req, res) => {
+    const { amount } = req.body; // Expect amount from client
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      });
+      res.status(200).json(paymentIntent);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+  // Example test route
   app.get("/test", async (req, res) => {
     const data = await Funko.find();
     const cleaned = data.map((funko) => funko._doc);
     console.log(cleaned);
     res.json(cleaned);
   });
-  // if we're in production, serve client/dist as static assets
+
+  // Serve static assets if in production
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
@@ -42,7 +58,6 @@ const startApolloServer = async () => {
       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
-
 
   db.once("open", () => {
     app.listen(PORT, () => {
